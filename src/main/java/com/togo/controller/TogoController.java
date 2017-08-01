0c5c22e24299c8d49be28bfce76de55c693afec7
luang <luang@bilibili.com>
2017-08-01 11:47:10 +0800

    渠道可用性监控

--- src/main/java/com/togo/controller/TogoController.java
+++ src/main/java/com/togo/controller/TogoController.java
@@ -44,8 +44,6 @@ public class TogoController {
     @GetMapping("/channels")
     public Map getUosdkChannelInfo() {
 
-        System.out.println(1);
-
         channelMap.forEach((String channelName, String hostString) -> {
             try {
                 boolean isReachable = InetAddress.getByName(hostString).isReachable(1000);
