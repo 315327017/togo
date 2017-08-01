e02e2d523c407d5e245fae5c982e5e0c45b25ae6
luang <luang@bilibili.com>
2017-08-01 14:56:15 +0800

    注释不支持PING命令渠道

--- src/main/java/com/togo/schedule/ChannelMoniter.java
+++ src/main/java/com/togo/schedule/ChannelMoniter.java
@@ -28,6 +28,10 @@ public class ChannelMoniter {
         });
     }
 
+    /**
+     * <p>监控渠道<p/>
+     * <p>注释渠道禁止PING命令，无法检测</p>
+     */
     private static final ImmutableMap<String,String> channelMap = new ImmutableMap.Builder<String,String>()
             .put("UC九游","sdk.g.uc.cn")
             .put("百度","querysdkapi.91.com")
@@ -36,17 +40,17 @@ public class ChannelMoniter {
             .put("新浪平台","m.game.weibo.cn")
             .put("178平台","passport.stargame.com")
             .put("棱镜","gameproxy.xinmei365.com")
-            .put("当乐","ngsdk.d.cn")
+//            .put("当乐","ngsdk.d.cn")
             .put("豌豆荚","pay.wandoujia.com")
             .put("OPPO","i.open.game.oppomobile.com")
-            .put("优酷","sdk.api.gamex.mobile.youku.com")
+//            .put("优酷","sdk.api.gamex.mobile.youku.com")
             .put("拇指玩","sdk.muzhiwan.com")
             .put("4399","m.4399api.com")
             .put("vivo","usrsys.inner.bbk.com")
             .put("华为","api.vmall.com")
-            .put("联想","passport.lenovo.com")
+//            .put("联想","passport.lenovo.com")
             .put("金立","id.gionee.com")
-            .put("酷派","openapi.coolyun.com")
+//            .put("酷派","openapi.coolyun.com")
             .put("斗鱼","www.douyutv.com")
             .put("今日头条","open.snssdk.com")
             .put("魅族","api.game.meizu.com")
