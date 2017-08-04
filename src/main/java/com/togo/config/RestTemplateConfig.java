a7e410e91e1cf3da19158e92121aafc201e7fa92
luang <luang@bilibili.com>
2017-08-04 14:34:37 +0800

    调通了

--- src/main/java/com/togo/config/RestTemplateConfig.java
+++ src/main/java/com/togo/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.togo.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -11,8 +12,9 @@ public class RestTemplateConfig {
     @Bean(name = "restTemplate")
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
-        f.setReadTimeout(1500);
-        f.setConnectTimeout(1500);
+        f.setReadTimeout(5000);
+        f.setConnectTimeout(5000);
+
         return new RestTemplate(f);
     }
 
--- src/main/java/com/togo/controller/QAController.java
+++ src/main/java/com/togo/controller/QAController.java
@@ -1,10 +1,13 @@
 package com.togo.controller;
 
+import com.google.common.collect.ImmutableMap;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
@@ -15,13 +18,19 @@ import java.util.Map;
 public class QAController {
 
 
-
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("/qa")
     public Map qa(HttpServletRequest request) throws UnsupportedEncodingException {
-        String url = "http://gamesdk-qa.biligame.com/qa?tohost="+URLEncoder.encode(request.getParameter("tohost"),"UTF-8");
-       return restTemplate.exchange(url,HttpMethod.valueOf(request.getMethod()),null,Map.class).getBody();
+        String url = "http://gamesdk-qa.biligame.com/qa?tohost=";
+        String[] strs = request.getQueryString().split("8080");
+        String host = strs[0].replace("tohost=", "");
+        String queryString = strs[1];
+        url += host + "8080" + queryString;
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://gamesdk-qa.biligame.com/qa");
+        builder.queryParam("tohost",host + "8080" + queryString);
+        return restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.valueOf(request.getMethod()), null, Map.class).
+                getBody();
     }
 }
